3f2d9b852980875a002031c9a7581696731b77e5
YuJun <clf0914@naver.com>
2025-05-20 22:12:48 +0900

    feat: 포켓몬 카드 컴포넌트 구현 및 선택 기능 추가

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { usePokemon } from "../context/PokemonContext";
+import {
+  Card,
+  CardImage,
+  PokemonImage,
+  CardContent,
+  PokemonName,
+  TypeContainer,
+  TypeBadge,
+  AddButton,
+} from "./styles/PokemonCardStyles";
+
+const PokemonCard = ({ pokemon, isSelected }) => {
+  const navigate = useNavigate();
+  // Context에서 함수 가져오기
+  const { addPokemon } = usePokemon();
+
+  const handleCardClick = () => {
+    navigate(`/pokemon/${pokemon.id}`);
+  };
+
+  const handleAddClick = (e) => {
+    e.stopPropagation();
+    addPokemon(pokemon);
+  };
+
+  return (
+    <Card>
+      <CardImage onClick={handleCardClick}>
+        <PokemonImage src={pokemon.image} alt={pokemon.name} />
+      </CardImage>
+      <CardContent>
+        <PokemonName>{pokemon.name}</PokemonName>
+        <TypeContainer>
+          {pokemon.type.map((type, index) => (
+            <TypeBadge key={index} type={type}>
+              {type}
+            </TypeBadge>
+          ))}
+        </TypeContainer>
+        <AddButton onClick={handleAddClick}>
+          {isSelected ? "추가됨" : "추가"}
+        </AddButton>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PokemonCard;
