4e2cc2dbb77af4faf5bc143480ec7be825e4263d
Yujun <clf0914@naver.com>
2025-05-22 17:30:51 +0900

    home ui 구성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import {
+  HomeContainer,
+  Title,
+  StartButton,
+  PokemonImage,
+} from "../components/styles/HomeStyles";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    navigate("/dex");
+  };
+
+  return (
+    <HomeContainer>
+      <PokemonImage
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
+        alt="Pokemon Logo"
+      />
+      <Title>포켓몬 도감</Title>
+
+      <StartButton onClick={handleStart}>포켓몬 도감 시작하기</StartButton>
+    </HomeContainer>
+  );
+};
+
+export default Home;
