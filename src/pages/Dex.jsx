899290ec83cd2664e8346336f21a38fa51aac990
Yujun <clf0914@naver.com>
2025-05-23 22:10:53 +0900

    home Button 컴포넌트 사용에 따른 Link 컴포넌트 삭제

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,34 +1,30 @@
-import { Link } from "react-router-dom";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
-import { MOCK_DATA } from "../data/mockData";
+import MOCK_DATA from "../data/mockData";
+import { usePokemon } from "../context/PokemonContext";
 import {
   DexContainer,
   DexHeader,
   DexTitle,
   DexContent,
   DexPageWrapper,
+  HomeButton,
 } from "../components/styles/DexStyles";
 
-const Dex = ({ selectedPokemons, addPokemon, removePokemon }) => {
+const Dex = () => {
+  const { selectedPokemons } = usePokemon();
+
   return (
     <DexPageWrapper>
       <DexContainer>
         <DexHeader>
           <DexTitle>포켓몬 도감</DexTitle>
-          <Link to="/">홈으로 돌아가기</Link>
+          <HomeButton to="/" />
         </DexHeader>
 
         <DexContent>
-          <Dashboard
-            selectedPokemons={selectedPokemons}
-            removePokemon={removePokemon}
-          />
-          <PokemonList
-            pokemons={MOCK_DATA}
-            selectedPokemons={selectedPokemons}
-            addPokemon={addPokemon}
-          />
+          <Dashboard />
+          <PokemonList pokemons={MOCK_DATA} />
         </DexContent>
       </DexContainer>
     </DexPageWrapper>
